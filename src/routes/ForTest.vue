f38841d91f5feebbdb60f02f81c2f37572918a8a
Yurile72 <eriol72@limeade.me>
2022-06-22 11:26:57 +0900

    edit

--- src/routes/ForTest.vue
+++ src/routes/ForTest.vue
@@ -1,20 +1,54 @@
 <template>
   <a-layout>
     <h2>수동조작용</h2>
-    <a-switch v-model:checked="checked" />
+    <button @click="change">변꺼어어어엉</button>
   </a-layout>
 </template>
-<script>
-import { defineComponent, ref } from "vue";
 
-export default defineComponent({
-  setup() {
-    const checked = ref(false);
+<script>
+import { getDatabase, ref, onValue, update } from "firebase/database";
+
+export default {
+  data: function () {
     return {
-      checked,
+      status: null,
     };
   },
-});
+  created() {
+    // for debug
+    // console.log(this.$firebase);
+    const db = getDatabase(this.$firebase);
+    // Use statusRef2 for Gala
+    // const statusRef = ref(db, "water_status");
+    // onValue(statusRef, (snapshot) => {
+    //   const data = snapshot.val();
+    //   this.waterStatus = data.ph_status;
+    //   console.log(data.ph_status);
+    // });
+    const statusRef2 = ref(db, "ph_status");
+    onValue(statusRef2, (snapshot) => {
+      const data = snapshot.val();
+      this.status = data;
+      console.log(data);
+    });
+  },
+
+  methods: {
+    // for debug
+    change: function () {
+      const db = getDatabase(this.$firebase);
+      // this.waterStatus = !this.waterStatus;
+      // set(ref(this.db, "/ph_status"), {
+      //   // this.waterStatus;
+      // });
+      const updates = {};
+      updates["/ph_status"] = !this.status;
+      // return update(ref(db), updates);
+      console.log(this.status);
+      update(ref(db), updates);
+    },
+  },
+};
 </script>
 
 <style></style>
